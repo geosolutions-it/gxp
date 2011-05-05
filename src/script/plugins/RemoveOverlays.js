bd6ed4ffa0bbb337c8c9693d133e445d3471167e
igrcic <ivan.grcic@geo-solutions.it>
2011-05-05 14:34:19 +0200

    Adding confirmation dialog before removing all the layers

--- src/script/plugins/RemoveOverlays.js
+++ src/script/plugins/RemoveOverlays.js
@@ -27,20 +27,26 @@ Ext.namespace("gxp.plugins");
  */
 gxp.plugins.RemoveOverlays = Ext.extend(gxp.plugins.Tool, {
     
-    /** api: ptype = gxp_removelayer */
+    /** api: ptype = gxp_removeoverlays */
     ptype: "gxp_removeoverlays",
     
-    /** api: config[removeMenuText]
+    /** api: config[removeOverlaysMenuText]
      *  ``String``
      *  Text for remove menu item (i18n).
      */
     removeOverlaysMenuText: "Remove overlays",
 
-    /** api: config[removeActionTip]
+    /** api: config[removeOverlaysActionTip]
      *  ``String``
      *  Text for remove action tooltip (i18n).
      */
     removeOverlaysActionTip: "Removes all overlays from the map",
+
+    /** api: config[removeOverlaysConfirmationText]
+     *  ``String``
+     *  Text that is diplayed inside inside delete confirmation window (i18n).
+     */
+    removeOverlaysConfirmationText: "Are you sure you want to remove all loaded overlays from the map?",
     
     /** api: method[addActions]
      */
@@ -52,13 +58,28 @@ gxp.plugins.RemoveOverlays = Ext.extend(gxp.plugins.Tool, {
             disabled: true,
             tooltip: this.removeOverlaysActionTip,
             handler: function() {
-                var layers = this.target.mapPanel.layers;
+            
+                var removeAction = function(buttonId, text,opt){
+                    if(buttonId === 'ok'){
+                        var layers = this.target.mapPanel.layers;
 
-                layers.data.each(function(record, index, totalItems ) {
-                    if(record.get('group') !== 'background'){
-                        layers.remove(record);
+                        layers.data.each(function(record, index, totalItems ) {
+                            if(record.get('group') !== 'background'){
+                                layers.remove(record);
+                            }
+                        }); 
                     }
-                });                
+                };
+                
+                Ext.Msg.show({
+                   title: this.removeOverlaysMenuText,
+                   msg: this.removeOverlaysConfirmationText,
+                   buttons: Ext.Msg.OKCANCEL,
+                   fn: removeAction,
+                   icon: Ext.MessageBox.QUESTION,
+                   scope: this
+                });
+                
             },
             scope: this
         }]);
