5aea280cad1d760ee974431351cb340ccde3c7f1
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2012-01-16 15:28:00 +0100

    externalized user/pass data with login plugin

--- src/script/plugins/SaveDefaultContext.js
+++ src/script/plugins/SaveDefaultContext.js
@@ -55,133 +55,76 @@ gxp.plugins.SaveDefaultContext = Ext.extend(gxp.plugins.Tool, {
     addActions: function() {
 		
 		var saveContext = new Ext.Button({
-		        id: "save-context-button",
-			      needsAuthorization: true,
+			id: "save-context-button",
+		    needsAuthorization: true,
             menuText: this.saveDefaultContextMenuText,
             iconCls: "gxp-icon-savedefaultcontext",
             disabled: true,
+			auth:undefined,
+			scope:this,
             tooltip: this.saveDefaultContextActionTip,
             handler: function() {
-            
-			  var app = this.target;
+				var app = this.target;
 				var xmlContext;
-				var handleSave = function(){
-					  var xmlContext = this.xmlContext;
-					  /*OpenLayers.Request.PUT({
-						  url: 'http://admin:1geosol2@demo1.geo-solutions.it/exist/rest/mapadmin/context.xml',
-						  data: xmlContext,
-						  user: 'admin',
-						  password: '1geosol2',
-						  //proxy: '',
-						  callback: function(request) {
-							  if (request.status == 201){
-								  Ext.Msg.show({
-									 title: this.contextSaveSuccessString,
-									 msg: request.statusText,
-									 buttons: Ext.Msg.OK,
-									 icon: Ext.MessageBox.OK,
-									 scope: this
-								  });
-							  } else {
-								  Ext.Msg.show({
-									 title:this.contextSaveFailString,
-									 msg: request.statusText,
-									 buttons: Ext.Msg.OK,
-									 icon: Ext.MessageBox.ERROR,
-									 scope: this
-								  });
-							  }
-						  },
-						  scope: this
-					  });*/
-					  
-            /*var xmlhttp = null;
-            var request = null;
-            try {
-              xmlhttp = new XMLHttpRequest();
-            } catch(e) {
-              try {
-                xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-              } catch(e) {
-                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-              }
-            }
-            
-            var mask = Ext.Msg;                      
-            xmlhttp.onreadystatechange = function requestHandler() {
-                  if (request.readyState == 4 && request.status == 201) {
-                      mask.show({
-                         title: "Context saved succesfully",
-                         msg: request.statusText,
-                         buttons: Ext.Msg.OK,
-                         icon: Ext.MessageBox.OK
-                      });
-                  }else if(request.readyState == 4){
-                      mask.show({
-                         title: "Context not saved succesfully",
-                         msg: request.statusText,
-                         buttons: Ext.Msg.OK,
-                         icon: Ext.MessageBox.ERROR
-                      });
-                  }
-            };
-            
-            request = xmlhttp;
-            request.open("PUT", "proxy/?url=http://admin:1geosol2@demo1.geo-solutions.it/exist/rest/mapadmin/context.xml");
-            request.send(xmlContext);*/
-            
-            var mask = new Ext.LoadMask(Ext.getBody(), {msg:"Please wait..."});
-					  mask.show();
-								
-            Ext.Ajax.request({
-               url: proxy + "http://admin:1geoadmin2@demo1.geo-solutions.it/exist/rest/mapadmin/context.xml",
-               method: 'PUT',
-               params: xmlContext,
-               scope: this,
-               success: function(response, opts){
-                  mask.hide();
-                  app.modified = false;
-                  //modified = false;
-                  Ext.Msg.show({
-                       title: this.contextSaveSuccessString,
-                       msg: response.statusText,
-                       buttons: Ext.Msg.OK,
-                       icon: Ext.MessageBox.OK
-								  });
-               },
-               failure:  function(response, opts){
-                  mask.hide();
-                  Ext.Msg.show({
-                     title: this.contextSaveFailString,
-                     msg: response.statusText,
-                     buttons: Ext.Msg.OK,
-                     icon: Ext.MessageBox.ERROR
-								  });
-               }
-            });		
+				var handleSave = function(auth){
+					var xmlContext = this.xmlContext;				
+					var mask = new Ext.LoadMask(Ext.getBody(), {msg:"Please wait..."});
+					mask.show();
+					alert(this.target.auth);
+					//var auth='Basic ' + Base64.encode(this.user+":"+this.pass);					
+					Ext.Ajax.request({
+						//** REMOVE USER AND PASS IN URL WHEN PROXY WILL SUPPORT BASIC AUTH **//
+						url: proxy + "http://"+this.target.user+":"+this.target.pass +"@"+"demo1.geo-solutions.it/exist/rest/mapadmin/context.xml",
+						headers: {
+							'Authorization' : this.target.auth
+						},
+						method: 'PUT',
+						params: xmlContext,
+						scope: this,
+						success: function(response, opts){
+						  mask.hide();
+						  app.modified = false;
+						  //modified = false;
+						  Ext.Msg.show({
+							   title: this.contextSaveSuccessString,
+							   msg: response.statusText,
+							   buttons: Ext.Msg.OK,
+							   icon: Ext.MessageBox.OK
+						  });
+						},
+						failure:  function(response, opts){
+						  mask.hide();
+						  Ext.Msg.show({
+							 title: this.contextSaveFailString,
+							 msg: response.statusText,
+							 buttons: Ext.Msg.OK,
+							 icon: Ext.MessageBox.ERROR
+						  });
+						}
+					});		
 				};
 				
 				var configStr = Ext.util.JSON.encode(app.getState());  
 				var url = app.xmlJsonTranslateService + "HTTPWebGISSave";
-				
+				alert(app.auth);
 				OpenLayers.Request.issue({
-            method: 'POST',
-            url: url,
-            data: configStr,
-            callback: function(request) {
-              if (request.status == 200) {
-                this.xmlContext = request.responseText;
-                handleSave.call(this);
-              } else {
-                //throw this.saveErrorText + request.responseText;
-                throw request.responseText;
-              }						
-            },
-            scope: this
-          });				
-   
-            },
-            scope: this
+					method: 'POST',
+					url: url,
+					data: configStr,
+					callback: function(request) {
+					  if (request.status == 200) {
+						this.xmlContext = request.responseText;
+						handleSave.call(this);
+					  } else {
+						//throw this.saveErrorText + request.responseText;
+						throw request.responseText;
+					  }						
+					},
+					scope: this
+			    });				
+		   
+			},
+			scope: this
         });
 		
         var actions = gxp.plugins.SaveDefaultContext.superclass.addActions.apply(this, [ saveContext ]);        
