51777e16a7537fc18b5296c720870366fd3d4b9e
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-11-08 17:14:59 +0100

    - Print Module Update

--- src/script/plugins/Print.js
+++ src/script/plugins/Print.js
@@ -247,7 +247,8 @@ gxp.plugins.Print = Ext.extend(gxp.plugins.Tool, {
             function isPrintable(layer) {
                 return layer.getVisibility() === true && (
                     layer instanceof OpenLayers.Layer.WMS ||
-                    layer instanceof OpenLayers.Layer.OSM
+                    layer instanceof OpenLayers.Layer.OSM ||
+                    layer instanceof OpenLayers.Layer.XYZ
                 );
             }
 
